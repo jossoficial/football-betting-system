35b08b33ebd8e1a8e08c55b9bb8e6e6ff6906403
jossoficial <jossoficial78@gmail.com>
2026-03-25 01:00:06 -0600

    Create ml_model.py

---
+++ ml_model.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+def train_model():
+    df = pd.read_csv("data/training.csv")
+
+    X = df.drop("result", axis=1)
+    y = df["result"]
+
+    model = RandomForestClassifier(n_estimators=100)
+    model.fit(X, y)
+
+    return model
+
+
+def predict_ml(model, features):
+    prob = model.predict_proba([features])[0][1]
+    return prob
