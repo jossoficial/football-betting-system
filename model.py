2cb9d423a8066012e77bd25b1eded470eb32b363
jossoficial <jossoficial78@gmail.com>
2026-03-24 23:17:48 -0600

    Update model.py

--- model.py
+++ model.py
@@ -1,13 +1,11 @@
 import numpy as np
-import pandas as pd
 from itertools import product
 
 def poisson_prob(lmbda, k):
     return (np.exp(-lmbda) * lmbda**k) / np.math.factorial(k)
 
-def match_prediction(home_xg, away_xg):
-    max_goals = 5
-
+def predict_match(home_xg, away_xg):
+    max_goals = 6
     matrix = np.zeros((max_goals, max_goals))
 
     for i, j in product(range(max_goals), range(max_goals)):
@@ -17,11 +15,13 @@ def match_prediction(home_xg, away_xg):
     draw = np.sum(np.diag(matrix))
     away_win = np.sum(np.triu(matrix, 1))
 
-    over_25 = np.sum(matrix[i][j] for i in range(max_goals) for j in range(max_goals) if i+j > 2)
+    over_25 = sum(matrix[i][j] for i in range(max_goals) for j in range(max_goals) if i+j > 2)
+    under_35 = sum(matrix[i][j] for i in range(max_goals) for j in range(max_goals) if i+j <= 3)
 
     return {
-        "home_win": round(home_win, 2),
-        "draw": round(draw, 2),
-        "away_win": round(away_win, 2),
-        "over_2_5": round(over_25, 2)
+        "home_win": home_win,
+        "draw": draw,
+        "away_win": away_win,
+        "over_2_5": over_25,
+        "under_3_5": under_35
     }
