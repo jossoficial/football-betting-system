da764aac7eef8fe9c29f0aee6aeb96683adf76f8
jossoficial <jossoficial78@gmail.com>
2026-03-24 23:18:11 -0600

    Update value.py

--- value.py
+++ value.py
@@ -1,23 +1,24 @@
-def find_value_bets(predictions):
-    # cuotas simuladas (luego conectamos API real)
-    odds = {
-        "home_win": 1.80,
-        "draw": 3.50,
-        "away_win": 4.00,
-        "over_2_5": 1.90
-    }
-
+def find_value(pred, odds):
     value_bets = []
 
-    for key in predictions:
-        prob = predictions[key]
-        implied = 1 / odds[key]
+    for key in pred:
+        if key not in odds or odds[key] is None:
+            continue
+
+        prob = pred[key]
+        odd = odds[key]
+
+        implied = 1 / odd
 
         if prob > implied:
-            value_bets.append({
-                "market": key,
-                "prob": prob,
-                "odds": odds[key]
-            })
+            edge = prob - implied
+
+            if edge > 0.05:
+                value_bets.append({
+                    "market": key,
+                    "prob": round(prob, 2),
+                    "odds": odd,
+                    "edge": round(edge, 2)
+                })
 
     return value_bets
