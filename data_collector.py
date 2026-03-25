74441eca3813fa256e03c2b2d2c50fd3a3eb3d8f
jossoficial <jossoficial78@gmail.com>
2026-03-25 01:23:43 -0600

    Update data_collector.py

--- data_collector.py
+++ data_collector.py
@@ -4,6 +4,8 @@ import os
 FILE = "storage/matches.csv"
 
 def save_match(home, away, home_xg, away_xg):
+    os.makedirs("storage", exist_ok=True)
+
     file_exists = os.path.isfile(FILE)
 
     with open(FILE, "a", newline="") as f:
