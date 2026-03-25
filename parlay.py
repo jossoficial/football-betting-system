e32603696109d44372059f466bb1b199d6bf4110
jossoficial <jossoficial78@gmail.com>
2026-03-25 01:04:43 -0600

    Update parlay.py

--- parlay.py
+++ parlay.py
@@ -1,23 +1,19 @@
 def build_parlays(all_values):
     parlays = []
 
-    # ordenar partidos por mejor pick
     sorted_matches = sorted(
         all_values,
         key=lambda x: x["values"][0]["score"] if x["values"] else 0,
         reverse=True
     )
 
-    # tomar solo los mejores 3 partidos
     top_matches = sorted_matches[:3]
-
-    parlay = []
 
     for match in top_matches:
         best_pick = match["values"][0]
-        parlay.append({
+        parlays.append({
             "match": match["match"],
             "pick": best_pick
         })
 
-    return parlay
+    return parlays
