f1485682b0de4111f0c55b75256c6edad26ff0b5
jossoficial <jossoficial78@gmail.com>
2026-03-25 02:19:26 -0600

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -3,9 +3,13 @@ from email.mime.text import MIMEText
 import os
 
 def send_email(picks):
-    sender = os.getenv("EMAIL_USER")
-    password = os.getenv("EMAIL_PASS")
-    receiver = os.getenv("EMAIL_TO")
+    sender = os.environ.get("EMAIL_USER")
+    password = os.environ.get("EMAIL_PASS")
+    receiver = os.environ.get("EMAIL_TO")
+
+    if not sender or not password or not receiver:
+        print("❌ ERROR: Secrets no cargados")
+        return
 
     if not picks:
         message = "No hay picks con valor hoy."
@@ -25,6 +29,10 @@ def send_email(picks):
     msg["From"] = sender
     msg["To"] = receiver
 
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-        server.login(sender, password)
-        server.sendmail(sender, receiver, msg.as_string())
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+            server.login(sender, password)
+            server.sendmail(sender, receiver, msg.as_string())
+            print("✅ Email enviado")
+    except Exception as e:
+        print("❌ ERROR EMAIL:", e)
